9978d7bd9ef714245f7aad036a7d940fcb98f625
Bartłomiej Cupiał <bartomiej.cupial@student.uj.edu.pl>
2022-12-04 12:50:19 +0100

    update fsh

--- models/frame_seed_generator.py
+++ models/frame_seed_generator.py
@@ -9,27 +9,27 @@ class FrameSeedGenerator(nn.Module):
     def __init__(self):
         super(FrameSeedGenerator, self).__init__()
         self.D = 512
-        self.fc1 = nn.Linear(2048, 1023)   # d = 2048
-        self.fc2 = nn.Linear(1024, 511)
-        self.fc3 = nn.Linear(512, 512)     # D = 512
+        self.fc1 = nn.Linear(2048 + self.D, 1023)
+        self.fc2 = nn.Linear(1024 + self.D, 511)
+        self.fc3 = nn.Linear(512 + self.D, 512)
 
     def forward(self, seeds, noise, time):
-        x = torch.hstack([time, seeds])
+        one_hot_len = noise.shape[1] - self.D
+        bs = noise.shape[0]
+        duration = time.shape[0] // bs
+        pro_noise = noise[:, :-one_hot_len].tile(duration, 1)
+
+        x = torch.hstack([pro_noise, time, seeds])
         x = torch.relu(self.fc1(x))
-        x = torch.hstack([time, x])
+        x = torch.hstack([pro_noise, time, x])
         x = torch.relu(self.fc2(x))
-        x = torch.hstack([time, x])
+        x = torch.hstack([pro_noise, time, x])
         x = self.fc3(x)
 
-        one_hot_len = noise.shape[1] - x.shape[1]
-        bs = noise.shape[0]
-        # x = x.reshape(bs, -1, 512)
-        # # we want to start from noise
-        # x = noise[:, :-one_hot_len].unsqueeze(1) + x - x[:, None, 0]
-        # x = x.reshape(-1, 512)
+        x = pro_noise + x - x[:, None, 0]
 
         # normalize
-        # x = x / LA.norm(x, dim=1, keepdim=True) * math.sqrt(self.D)
+        x = x / LA.norm(x, dim=1, keepdim=True) * math.sqrt(self.D)
         # append one hot for generator
         one_hots = noise[:, -one_hot_len:]
         x = torch.cat([x, one_hots.repeat(x.shape[0] // one_hots.shape[0], 1)], dim=1)
